9c5b6cb8a3c3f5a2a5bbef41805a1fd3e4e54ea8
mhselva <mhselva@wpi.edu>
2020-05-08 20:23:33 -0400

    forgot to commit observer tests before oops

--- src/test/java/ObserverTests.java
+++ src/test/java/ObserverTests.java
@@ -1,6 +1,80 @@
+import edu.wpi.cs3733.entity.CaesarCipher;
+import edu.wpi.cs3733.entity.ElbonianCipher;
+import edu.wpi.cs3733.entity.Message;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Tests for your observer pattern; make sure your code works!
  * Test-driven development is a good idea here.
  */
 public class ObserverTests {
+    @Test
+    public void testObserver() {
+        Message cleartext = new Message();
+        CaesarCipher caesarCipher = new CaesarCipher();
+        ElbonianCipher elbonianCipher = new ElbonianCipher();
+        cleartext.register(caesarCipher);
+        cleartext.register(elbonianCipher);
+        cleartext.setText("gnitset");
+        String caesarBefore = caesarCipher.getText();
+        String elbonianBefore = elbonianCipher.getText();
+        cleartext.setText("testing");
+        Assert.assertNotEquals(caesarBefore, caesarCipher.getText());
+        Assert.assertNotEquals(elbonianBefore, elbonianCipher.getText());
+    }
+    @Test
+    public void testObserverEmpty() {
+        Message cleartext = new Message();
+        CaesarCipher caesarCipher = new CaesarCipher();
+        ElbonianCipher elbonianCipher = new ElbonianCipher();
+        cleartext.register(caesarCipher);
+        cleartext.register(elbonianCipher);
+        cleartext.setText("");
+        String caesarBefore = caesarCipher.getText();
+        String elbonianBefore = elbonianCipher.getText();
+        cleartext.setText("");
+        Assert.assertEquals(caesarBefore, caesarCipher.getText());
+        Assert.assertEquals(elbonianBefore, elbonianCipher.getText());
+    }
+    @Test
+    public void testObserverSame() {
+        Message cleartext = new Message();
+        CaesarCipher caesarCipher = new CaesarCipher();
+        ElbonianCipher elbonianCipher = new ElbonianCipher();
+        cleartext.register(caesarCipher);
+        cleartext.register(elbonianCipher);
+        cleartext.setText("test");
+        String caesarBefore = caesarCipher.getText();
+        String elbonianBefore = elbonianCipher.getText();
+        cleartext.setText("test");
+        Assert.assertEquals(caesarBefore, caesarCipher.getText());
+        Assert.assertEquals(elbonianBefore, elbonianCipher.getText());
+    }
+    @Test
+    public void testCaesarObserver() {
+        Message cleartext = new Message();
+        CaesarCipher caesarCipher = new CaesarCipher();
+        ElbonianCipher elbonianCipher = new ElbonianCipher();
+        cleartext.register(caesarCipher);
+        cleartext.setText("test");
+        String caesarBefore = caesarCipher.getText();
+        String elbonianBefore = elbonianCipher.getText();
+        cleartext.setText("testing");
+        Assert.assertNotEquals(caesarBefore, caesarCipher.getText());
+        Assert.assertEquals(elbonianBefore, elbonianCipher.getText());
+    }
+    @Test
+    public void testElbonianObserver() {
+        Message cleartext = new Message();
+        CaesarCipher caesarCipher = new CaesarCipher();
+        ElbonianCipher elbonianCipher = new ElbonianCipher();
+        cleartext.register(elbonianCipher);
+        cleartext.setText("test");
+        String caesarBefore = caesarCipher.getText();
+        String elbonianBefore = elbonianCipher.getText();
+        cleartext.setText("testing");
+        Assert.assertEquals(caesarBefore, caesarCipher.getText());
+        Assert.assertNotEquals(elbonianBefore, elbonianCipher.getText());
+    }
 }
